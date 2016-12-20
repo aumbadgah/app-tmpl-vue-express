fbe48d45980fc8a99c830feda852b9d21b61fa48
Antti Suoninen <antti.suoninen@gmail.com>
2016-12-20 21:03:14 +0200

    nested vue component, get props with api call

---
+++ lib/front/src/components/messageHeader.vue
@@ -0,0 +1,22 @@
+
+<style lang="less">
+    .message-header {
+        font-family: Arial, sans-serif;
+    }
+</style>
+
+<template>
+    <h1 class="message-header red">{{message}}</h1>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+            }
+        },
+        props: [
+            'message'
+        ]
+    }
+</script>
